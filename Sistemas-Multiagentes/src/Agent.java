bfb6d60643b87dc0a7899fdc4425fd794654b10b
Antonio <Antonio@Antonio>
2019-09-25 19:47:32 +0200

    Incluida libreria y metodo de prueba.

--- Sistemas-Multiagentes/src/Agent.java
+++ Sistemas-Multiagentes/src/Agent.java
@@ -1,11 +1,16 @@
 
 /* Clase agente encargada de recopilar informacion de la web */
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class Agent {
 
-	public static void main(String[] args) {
-		
-		
+	void conectar() throws IOException {
+
+		Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
 
 	}
 
