00a1d5575dd9b722b4037a33cb590f0ce803f641
Antonio <Antonio@Antonio>
2019-09-25 20:12:00 +0200

    Implementado contador de palabras que empiezan por mayuscula.

--- Sistemas-Multiagentes/src/Agent.java
+++ Sistemas-Multiagentes/src/Agent.java
@@ -3,15 +3,34 @@
 
 import java.io.IOException;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class Agent {
 
 	void conectar() throws IOException {
-
+		
 		Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-		System.out.println(doc.title());
+		
+		// Body and pattern to be found
+		String body = doc.body().text();
+		String myPattern = "[A-Z]\\w*";
+		
+		// Pattern object and matcher creation
+		Pattern p = Pattern.compile(myPattern);
+		Matcher m = p.matcher(body);
+		
+		// Find pattern and count occurrences
+		int c = 0;
+		while(m.find()) {
+			System.out.println(m.group(0));
+			c++;
+		}
+		
+		System.out.println("\n** Palabras que empiezan por mayuscula: " + c + " **");	
 	}
 
 }
