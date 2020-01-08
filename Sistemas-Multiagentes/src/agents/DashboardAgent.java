08556b28297bf44108ec02d5199b545eeed67169
manjavacas <Antonio.Manjavacas@alu.uclm.es>
2020-01-08 19:40:43 +0100

    Implemented some Dashboard methods

--- Sistemas-Multiagentes/src/agents/DashboardAgent.java
+++ Sistemas-Multiagentes/src/agents/DashboardAgent.java
@@ -4,6 +4,7 @@ import jade.core.Agent;
 import jade.core.AID;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+
 import jade.proto.AchieveREResponder;
 import jade.proto.AchieveREInitiator;
 
@@ -16,36 +17,45 @@ public class DashboardAgent extends Agent {
 
     protected void setup() {
 
+        // TO-DO: URLs tokenizer
+        String msgContent = "";
+
+        ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+        msg.addReceiver(new AID("processerID", AID.ISLOCALNAME));
+        msg.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
+        msg.setContent(msgContent);
     }
 
     protected void takeDown() {
-   
+        System.out.println("[DASHBOARD-AGENT] Taking down...");
     }
 
-    public class DemandBehaviour extends AchieveREInitiator {
+    public class RequestInitiator extends AchieveREInitiator {
 
-        public DemandBehaviour(Agent a, MessageTemplate temp) {
+        public RequestInitiator(Agent a, MessageTemplate temp) {
             super(a, temp);
         }
 
         protected void handleAgree(ACLMessage agree) {
-            // TO-DO
+            System.out.println("[DASHBOARD-AGENT] " + agree.getSender().getName() + " has accepted the request.");
         }
 
-        protected void handleRefuse(ACLMessage refuese) {
-            // TO-DO
+        protected void handleRefuse(ACLMessage refuse) {
+            System.out.println("[DASHBOARD-AGENT] " + agree.getSender().getName() + " has rejected the request.");
         }
 
         protected void handleNotUnderstood(ACLMessage notUnderstood) {
-            // TO-DO
+            System.out.println("[DASHBOARD-AGENT] " + agree.getSender().getName() + " didn't understood the request.");
         }
 
         protected void handleInform(ACLMessage inform) {
-            // TO-DO
+            System.out.println("[DASHBOARD-AGENT] Received results from " + agree.getSender().getName());
+            // TO-DO: get results from message
+            // TO-DO: implement interface with results
         }
 
         protected void handleFailure(ACLMessage failure) {
-            // TO-DO
+            System.out.println("[DASHBOARD-AGENT] " + agree.getSender().getName() + " has failed!");
         }
 
     }
